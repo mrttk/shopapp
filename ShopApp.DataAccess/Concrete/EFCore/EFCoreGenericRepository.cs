44c0765e0dc8a387ff13c4ec261998ab6e098096
= <=>
2021-10-07 02:25:51 +0300

    Add generic repository

---
+++ ShopApp.DataAccess/Concrete/EFCore/EFCoreGenericRepository.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAccess.Abstract;
+
+namespace ShopApp.DataAccess.Concrete.EFCore
+{
+    public class EFCoreGenericRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext, new()
+    {
+        public void Create(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
