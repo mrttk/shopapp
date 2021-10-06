2b1cec0c5698cb465aa988c7b4c9c4f152a9066b
= <=>
2021-10-07 02:04:15 +0300

    Add generic base repository

---
+++ ShopApp.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace ShopApp.DataAccess.Abstract
+{
+    public interface IRepository<TEntity>
+    {
+        TEntity GetById(int id);
+        List<TEntity> GetAll();
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+    }
+}
