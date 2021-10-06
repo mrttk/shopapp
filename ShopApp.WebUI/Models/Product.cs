526bb1d91e72f456fd816feda903b017d37f7bd1
= <=>
2021-10-06 03:36:13 +0300

    Add data validation for product class

--- ShopApp.WebUI/Models/Product.cs
+++ ShopApp.WebUI/Models/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,19 @@ namespace ShopApp.WebUI.Models
     public class Product
     {
         public int ProductId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength =2)]
         public string Name { get; set; }
-        public double Price { get; set; }
+        [Required]
+        [Range(1,35000)]
+        public double? Price { get; set; }
         public string Description { get; set; }
-
+        
+        [Required]
         public string ImageUrl { get; set; }
+        
         public bool IsApproved { get; set; }
-        public int CategoryId { get; set; }
+        [Required]
+        public int? CategoryId { get; set; }
     }
 }
