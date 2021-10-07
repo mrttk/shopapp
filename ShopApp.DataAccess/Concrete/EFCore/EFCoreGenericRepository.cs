c9ff69456946e35ce5334dd36a9c9c405b2c0798
= <=>
2021-10-07 14:09:49 +0300

    Add generic repository implementations

--- ShopApp.DataAccess/Concrete/EFCore/EFCoreGenericRepository.cs
+++ ShopApp.DataAccess/Concrete/EFCore/EFCoreGenericRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using ShopApp.DataAccess.Abstract;
 
@@ -10,27 +11,45 @@ namespace ShopApp.DataAccess.Concrete.EFCore
     {
         public void Create(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            using (var context = new TContext())
+            {
+                context.Set<TEntity>().Add(entity);
+                context.SaveChanges();
+            }
         }
 
         public void Delete(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            using(var context = new TContext())
+            {
+                context.Set<TEntity>().Remove(entity);
+                context.SaveChanges();
+            }
         }
 
         public List<TEntity> GetAll()
         {
-            throw new System.NotImplementedException();
+            using(var context = new TContext())
+            {
+                return context.Set<TEntity>().ToList();
+            }
         }
 
         public TEntity GetById(int id)
         {
-            throw new System.NotImplementedException();
+            using(var context = new TContext())
+            {
+                return context.Set<TEntity>().Find(id);
+            }
         }
 
         public void Update(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            using(var context = new TContext())
+            {
+                context.Entry(entity).State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
--- ShopApp.DataAccess/Concrete/EFCore/EFCoreProductRepository.cs
+++ ShopApp.DataAccess/Concrete/EFCore/EFCoreProductRepository.cs
@@ -4,36 +4,9 @@ using ShopApp.Entity;
 
 namespace ShopApp.DataAccess.Concrete.EFCore
 {
-    public class EFCoreProductRepository : IProductRepository
+    public class EFCoreProductRepository : EFCoreGenericRepository<Product, ShopContext>, IProductRepository
     {
-        private ShopContext _db = new ShopContext();
-        public void Create(Product entity)
-        {
-            _db.Products.Add(entity);
-            _db.SaveChanges();
-        }
-
-        public void Delete(Product entity)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public List<Product> GetAll()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Product GetById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public List<Product> GetPopularProducts()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Update(Product entity)
         {
             throw new System.NotImplementedException();
         }
