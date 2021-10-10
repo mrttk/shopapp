9f0a27eabce9ce620f01ed4db8e19580f8d2aa46
= <=>
2021-10-10 19:32:56 +0300

    Add Category Repository

---
+++ ShopApp.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using ShopApp.Entity;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        Category GetById(int id);
+        List<Category> GetAll();
+        void Create(Category entity);
+        void Update(Category entity);
+        void Delete(Category entity);
+    }
+}
