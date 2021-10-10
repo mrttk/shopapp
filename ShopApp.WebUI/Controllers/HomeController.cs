8cb09cbc6067914505343445359a1b60357974b9
= <=>
2021-10-10 20:38:31 +0300

    Add IProductService Injection

--- ShopApp.WebUI/Controllers/HomeController.cs
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ShopApp.WebUI.Data;
-using ShopApp.WebUI.Models;
+using ShopApp.Business.Abstract;
+using ShopApp.DataAccess.Abstract;
 using ShopApp.WebUI.ViewModels;
 
 namespace ShopApp.WebUI.Controllers
 {
     public class HomeController:Controller
     {
+        private IProductService _productService;
+        public HomeController(IProductService productService)
+        {
+            this._productService = productService;
+        }
         public IActionResult Index()
         {
-            
 
-            var productViewModel = new ProductViewModel()
+            var productListViewModel = new ProductListViewModel()
             {
-                Products = ProductRepository.Products
+                Products = _productService.GetAll()
             };
 
-            return View(productViewModel);
+            return View(productListViewModel);
         } 
         public IActionResult About()
         {
             return View();
-        } 
+        }
     }
 }
