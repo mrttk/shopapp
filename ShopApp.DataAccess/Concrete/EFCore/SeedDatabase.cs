211b6bf6c899de36cb4cc6b909cf42069c6ba96c
= <=>
2021-10-12 22:36:56 +0300

    Update SeedDatabase file for Url columns

--- ShopApp.DataAccess/Concrete/EFCore/SeedDatabase.cs
+++ ShopApp.DataAccess/Concrete/EFCore/SeedDatabase.cs
@@ -26,15 +26,15 @@ namespace ShopApp.DataAccess.Concrete.EFCore
         }
 
         private static Category[] Categories = {
-            new Category() { Name = "Phones" },
-            new Category() { Name = "Computer" },
-            new Category() { Name = "Electronic" }
+            new Category() { Name = "Phones", Url = "phones" },
+            new Category() { Name = "Computer", Url = "computer" },
+            new Category() { Name = "Electronic", Url = "electronic" }
         };
         private static Product[] Products = {
-            new Product() { Name = "IPhone 6S", Price = 2000, ImageUrl = "1.jpg", Description = "Good Phone", IsApproved = true },
-            new Product() { Name = "IPhone 7", Price = 3000, ImageUrl = "2.jpg", Description = "Good Phone", IsApproved = true },
-            new Product() { Name = "IPhone 8", Price = 4000, ImageUrl = "3.jpg", Description = "Good Phone", IsApproved = false },
-            new Product() { Name = "IPhone 9", Price = 5000, ImageUrl = "1.jpg", Description = "Better Phone", IsApproved = true }
+            new Product() { Name = "IPhone 6S", Url="iphone-6s",Price = 2000, ImageUrl = "1.jpg", Description = "Good Phone", IsApproved = true },
+            new Product() { Name = "IPhone 7", Url="iphone-7", Price = 3000, ImageUrl = "2.jpg", Description = "Good Phone", IsApproved = true },
+            new Product() { Name = "IPhone 8", Url="iphone-8", Price = 4000, ImageUrl = "3.jpg", Description = "Good Phone", IsApproved = false },
+            new Product() { Name = "IPhone 9", Url="iphone-9",Price = 5000, ImageUrl = "1.jpg", Description = "Better Phone", IsApproved = true }
         };
 
         private static ProductCategory[] ProductCategories = {
