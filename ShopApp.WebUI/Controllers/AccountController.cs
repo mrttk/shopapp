5f16e7c8691a2a71438926e6342e2dade6084d0c
= <=>
2021-11-03 20:23:01 +0200

    Add EmailSender feature for Confirm Email

--- ShopApp.WebUI/Controllers/AccountController.cs
+++ ShopApp.WebUI/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShopApp.WebUI.EmailServices;
 using ShopApp.WebUI.Identity;
 using ShopApp.WebUI.Models;
 
@@ -11,10 +14,12 @@ namespace ShopApp.WebUI.Controllers
     {
         private UserManager<User> _userManager;
         private SignInManager<User> _signInManager;
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        private IEmailSender _emailSender; 
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager,IEmailSender emailSender)
         {
             this._userManager = userManager;
             this._signInManager = signInManager;
+            this._emailSender = emailSender;
         }
         
         public IActionResult Login(string ReturnUrl = null)
@@ -38,6 +43,12 @@ namespace ShopApp.WebUI.Controllers
                 ModelState.AddModelError("","Wrong password or user name!");
                 return View(model);
             }
+
+            // if (!await _userManager.IsEmailConfirmedAsync(user))
+            // {
+            //     ModelState.AddModelError("","We have sent you an e-mail to activate your membership. Please confirm your subscription.");
+            //     return View(model);
+            // }
 
             var result = await _signInManager.PasswordSignInAsync(user, model.Password, true, false);
 
@@ -73,6 +84,15 @@ namespace ShopApp.WebUI.Controllers
 
             if (result.Succeeded)
             {
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                //generate token
+                var url = Url.Action("ConfirmEmail","Account", new {
+                    userId = user.Id,
+                    token = code
+                });
+                Console.WriteLine(url);
+                //email
+                await _emailSender.SendEmailAsync(model.Email, "Confirm your account", $"Please <a href='http://localhost:5000{url}'>click</a> the link to confirm your email account.");
                 return RedirectToAction("Login","Account");
             }
             ModelState.AddModelError("","An unknown error has occurred. Please try again");
@@ -85,6 +105,36 @@ namespace ShopApp.WebUI.Controllers
             return Redirect("~/");
         }
 
+        public async Task<IActionResult> ConfirmEmail (string userId, string token)
+        {
+            if (userId == null || token == null)
+            {
+                CreateMessage("Invalid token","danger");
+                return View();
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user,token);
+                if (result.Succeeded)
+                {
+                    CreateMessage("The user has been confirmed!","success");
+                    return View();
+                }            
+            }
+            CreateMessage("The user is not registered.","warning");
+            return View();
+        }
 
+        private void CreateMessage(string message,string alerttype)
+        {
+            var info = new AlertMessage()
+            {
+                Message = message,
+                AlertType = alerttype
+            };
+
+            TempData["message"] = JsonConvert.SerializeObject(info);
+        }
     }
 }
