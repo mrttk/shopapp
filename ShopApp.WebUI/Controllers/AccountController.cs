324f4587e4eb4a9e7b16a8f2188b6f85595d77f4
= <=>
2021-11-07 18:21:43 +0200

    Add AccessDenied page for access management

--- ShopApp.WebUI/Controllers/AccountController.cs
+++ ShopApp.WebUI/Controllers/AccountController.cs
@@ -201,5 +201,11 @@ namespace ShopApp.WebUI.Controllers
 
             return View(model);
         }
+
+        public IActionResult AccessDenied()
+        {
+            
+            return View();
+        }
     }
 }
