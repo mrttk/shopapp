5571195a71536075d099086d2bfa3bb018c5b4cf
= <=>
2021-11-09 21:25:45 +0200

    Add Cart Entities and Migrations

---
+++ ShopApp.Entity/CartItem.cs
@@ -0,0 +1,13 @@
+namespace ShopApp.Entity
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CartId { get; set; }
+        public int Quantity { get; set; }
+
+        public Product Product { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
