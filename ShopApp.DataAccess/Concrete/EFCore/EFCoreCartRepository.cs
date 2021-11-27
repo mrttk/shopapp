dab92bba05b405e0a4d3f8c15dfdc34199c75c56
= <=>
2021-11-28 01:22:54 +0200

    Move connectionstring to application.json

--- ShopApp.DataAccess/Concrete/EFCore/EFCoreCartRepository.cs
+++ ShopApp.DataAccess/Concrete/EFCore/EFCoreCartRepository.cs
@@ -5,43 +5,40 @@ using ShopApp.Entity;
 
 namespace ShopApp.DataAccess.Concrete.EFCore
 {
-    public class EFCoreCartRepository : EFCoreGenericRepository<Cart, ShopContext>, ICartRepository
+    public class EFCoreCartRepository : EFCoreGenericRepository<Cart>, ICartRepository
     {
+        public EFCoreCartRepository(ShopContext context) : base(context)
+        {
+            
+        }
+
+        private ShopContext ShopContext
+        {
+            get { return context as ShopContext; }
+        }
         public void ClearCart(int cartId)
         {
-            using (var context  = new ShopContext())
-            {
-                var command = @"delete from CartItems where CartId=@p0";
-                context.Database.ExecuteSqlRaw(command, cartId);
-            }
+            var command = @"delete from CartItems where CartId=@p0";
+            ShopContext.Database.ExecuteSqlRaw(command, cartId);
         }
 
         public void DeleteFromCart(int cartId, int productId)
         {
-            using (var context  = new ShopContext())
-            {
-                var command = @"delete from CartItems where CartId=@p0 and ProductId=@p1";
-                context.Database.ExecuteSqlRaw(command, cartId, productId);
-            }
+            var command = @"delete from CartItems where CartId=@p0 and ProductId=@p1";
+            ShopContext.Database.ExecuteSqlRaw(command, cartId, productId);
         }
 
         public Cart GetByUserId (string userId)
         {
-            using (var context = new ShopContext())
-            {
-                return context.Carts
-                            .Include(i=>i.CartItems)
-                            .ThenInclude(i=>i.Product)
-                            .FirstOrDefault(i=>i.UserId == userId);
-            }
+            return ShopContext.Carts
+                        .Include(i=>i.CartItems)
+                        .ThenInclude(i=>i.Product)
+                        .FirstOrDefault(i=>i.UserId == userId);
         }
         public override void Update(Cart entity)
         {
-            using(var context = new ShopContext())
-            {
-                context.Carts.Update(entity);
-                context.SaveChanges();
-            }
+            ShopContext.Carts.Update(entity);
+            ShopContext.SaveChanges();
         }
     }
 }
