c8c4d209b88c7ca528c145700fcbb1a8924f2d56
= <=>
2021-11-28 16:11:04 +0200

    Add migration manager

--- ShopApp.WebUI/Program.cs
+++ ShopApp.WebUI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShopApp.WebUI.Extensions;
 
 namespace ShopApp.WebUI
 {
@@ -13,7 +14,7 @@ namespace ShopApp.WebUI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
